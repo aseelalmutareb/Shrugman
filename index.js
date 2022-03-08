5682111d91eff553b871d5fe773a65e6fbdb2ece
Aseel <92477499+aseelalmutareb@users.noreply.github.com>
2022-03-08 11:54:12 +0100

    added project files

---
+++ index.js
@@ -0,0 +1,62 @@
+const prompt = require('prompt-sync')({ sigint: true }); 
+require('colors');
+const Shrugman = require("./shrugman");
+
+const game = new Shrugman();
+
+
+let category = prompt(`Choose a category (movies / books): `.rainbow);
+game.setCategory(category);
+
+let guess = "";
+
+const runGame = function(){
+while(game.isGameOn()){
+    console.clear();
+    console.log("\n\n", game.displayGuessWord().yellow, "\n\n");
+    console.log(game.displayShrugman().bold.magenta, "\n\n")
+    guess = prompt(`Guess a letter or type entire name to solve it: `.yellow);
+    game.validateGuess(guess);
+}};
+
+runGame(); 
+
+console.clear();
+console.log("\n\n", game.displayGuessWord().yellow, "\n\n");
+console.log(game.displayShrugman().bold.magenta, "\n\n");
+
+if(game.isWinning()){
+    console.log(String.fromCodePoint(0x1F3C6, 0x1F973), "Well done!\n".magenta);
+}
+else{
+    console.log(String.fromCodePoint(0x1F91E, 0x1F62C),`Better luck next time: ${game.guessWord}\n`.magenta);
+}
+
+game.addResult();
+
+let anotherRound = prompt(`Another round with ${category} (y)? `.blue);
+
+if(anotherRound === "y"){
+    game.reset(); 
+    runGame();
+    console.clear();
+    console.log("\n\n", game.displayGuessWord().yellow, "\n\n");
+    console.log(game.displayShrugman().bold.magenta, "\n\n");
+    game.addResult();
+
+    if(game.isWinning()){
+        console.log(String.fromCodePoint(0x1F3C6, 0x1F973), "Well done!\n".magenta);
+    }
+    else
+        console.log(String.fromCodePoint(0x1F91E, 0x1F62C),`Better luck next time: ${game.guessWord}\n`.magenta);
+    
+}
+
+
+console.log(game.getResults(), "\n");
+console.log("-- THE END --".rainbow);
+    
+
+
+
+
